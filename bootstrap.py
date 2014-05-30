c091d4093e1e4b2322e545e5df5ec844c7a3b5f8
Justin <justin@devjustinian.com>
2014-05-30 00:28:01 -0700

    Better multi-platform handling of externals, and better messaging when a new package requires installation. This does require re-running install_externals.py though, sorry.

--- bootstrap.py
+++ bootstrap.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from os.path import abspath, dirname, join
 
 base_dir = abspath( dirname(__file__) )
@@ -6,10 +8,14 @@ conf_dir = join( base_dir, "conf" )
 data_dir = join( base_dir, "data" )
 logs_dir = join( base_dir, "logs" )
 
-import sys
-sys.path.append( join(base_dir,'lib') )
-sys.path.insert( 0, join(base_dir,'external') )
+ext_dir = join( base_dir, 'external', os.name )
+sys.path.insert( 0, join(base_dir,'lib') )
+sys.path.insert( 0, ext_dir )
 
-from pkg_resources import require
+from pkg_resources import require, DistributionNotFound
 for requirement in file( 'packages.txt' ):
-	require( requirement )
+	try:
+		require( requirement )
+	except DistributionNotFound:
+		print "Required package '%s' not found. Please run install_external.py." % (requirement.strip(),)
+		sys.exit(1)
--- install_external.py
+++ install_external.py
@@ -2,12 +2,20 @@
 
 import os
 import sys
-sys.path.append( 'lib' )
-sys.path.insert( 0, 'external' )
+from os.path import join
 
-os.environ['PYTHONPATH'] = 'external'
+ext_dir = join( 'external', os.name )
+try:
+	os.mkdir(ext_dir)
+except OSError:
+	pass
+
+sys.path.append( 'lib' )
+sys.path.insert( 0, ext_dir )
+
+os.environ['PYTHONPATH'] = ext_dir
 
 from setuptools.command import easy_install
 for requirement in file('packages.txt'):
 	if requirement.strip().startswith('#'): continue 
-	easy_install.main( ['--install-dir=external', requirement] )
+	easy_install.main( ['--install-dir='+ext_dir, requirement] )
